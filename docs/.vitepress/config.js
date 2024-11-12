60bfe1add474c397b17d64a3c873077f0668bb50
xxllff <xlf2023@sjtu.edu.cn>
2024-11-13 00:19:42 +0800

    更新配置文件，移除冗余注释并添加网站图标配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,10 +1,12 @@
-// docs/.vitepress/config.js
 export default {
-  // 站点级选项
-  title: "晨光红茶的博客", // 网站标题
-  description: "徐龙飞来写博客的地方", // 网站描述
+  title: "晨光红茶的博客",
+  description: "徐龙飞来写博客的地方",
   base: '/my-vitepress-blog/',
-  // outDir: '../public',
+
+  head: [
+    ["link", { rel: "icon", href: `/portfolio.ico` }],
+  ],
+
   themeConfig: {
     nav: [
       {
@@ -16,10 +18,6 @@ export default {
         ]
       },
       { text: '关于', link: '/about/experience.md' },
-    ],
-    head: [
-      // 配置网站的图标（显示在浏览器的 tab 上）
-      ["link", { rel: "icon", href: `/portfolio.ico` }],
     ],
     docFooter: {
       prev: "上一篇",
@@ -47,4 +45,3 @@ export default {
     },
   },
 };
-
